b8b2c81418ac9874c9da16a6fa06fc097f687204
samuelytt <samuaelyeshambel@gmail.com>
2026-02-26 00:44:58 +0300

    Now i know java might be more complex and memory needing than c++

--- Calc.java
+++ Calc.java
@@ -1,11 +1,42 @@
 // Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
-public class Calc {
-   public Calc() {
-   }
 
-   public static void main(String[] var0) {
+import java.util.Scanner;
+public class Calc{
+      public static void main(String[] var0) {
+     
       System.out.println("Hello, World!");
+       String name="calculater";
+       int choice;
+
+       Scanner sc = new Scanner(System.in);
+       do{ 
+            System.out.println("=====Menu=====");
+            System.out.print("1.Addition");
+            System.out.print("2.Multi");
+            System.out.print("3.Div");
+            System.out.print("4.Sub");
+            System.out.print("5. Exit")
+
+            choice= sc.nextInt();
+
+
+            System.out.print("Enter number 1: ");
+            int num = sc.nextInt(); 
+            
+            System.out.print("Enter number 2: ");
+            int num2 = sc.nextInt(); 
+
+            switch(choice){
+               case 1:
+                  System.out.println(num + num2);
+                           }
+
+            
+           
+       }while(choice != 5);
+
       
       
    }
 }
+
