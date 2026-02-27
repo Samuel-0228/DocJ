1feb43690c40e282142c3d914b75a16d66325f9e
samuelytt <samuaelyeshambel@gmail.com>
2026-02-27 21:49:30 +0300

    update 2.0

--- prob.java
+++ prob.java
@@ -1,19 +1,17 @@
-public class CoinToss {  // Fixed class name
-    // REQUIRED flip() function
+public class CoinToss {  
+
     public static int flip() {
-        return (int) (Math.random() * 2);  // 0=tails, 1=heads
+        return (int) (Math.random() * 2);  
     }
     
     public static void main(String[] args) {
         int heads = 0, tails = 0;
         
-        // Toss 100 times
+      
         for (int i = 0; i < 100; i++) {
-            int face = flip();  // Call function
-            
-            if (face == 1) {    // Problem says 1=heads
-                System.out.println("Heads");  // Print each toss
-                heads++;
+            int face = flip(); 
+            if (face == 1) {    
+                System.out.println("Heads");  
             } else {
                 System.out.println("Tails");
                 tails++;
