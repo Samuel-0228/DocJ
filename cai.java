848ad60a295825c1eadebf69fb7d7fb67ea167fb
samuelytt <samuaelyeshambel@gmail.com>
2026-02-28 22:41:53 +0300

    Update: latest changes

--- cai.java
+++ cai.java
@@ -5,26 +5,57 @@ public class cai {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
+        int correctanswer=0;
+        int answer=0;
+        
+       
         
         do {
             System.out.println("Enter Your choice: ");
             System.out.println("1. Add");
-            System.out.println("2. Exit");
+            System.out.println("2. Multiply");
+            System.out.println("3. Subtract");
+            System.out.println("4. Divide");
+            System.out.println("5. Exit");
             int choice = sc.nextInt();
             
-            if(choice == 2) break;
+            if(choice == 5) break;
             
-            int num1 = rand.nextInt(10) + 1;
+            int num1 = rand.nextInt(10)+1;
             int num2 = rand.nextInt(10) + 1;
+
+         switch(choice){ 
+            case 1: 
+                System.out.println("What is " + num1 + " plus " + num2 + "? ");
+                answer = sc.nextInt();
+                correctanswer=num1+num2;
+                break;
+            case 2:
+                System.out.println("What is " + num1 + " times " + num2 + "? ");
+                answer = sc.nextInt();
+                correctanswer=num1*num2;
+                break;
+            case 3:
+                System.out.println("What is " + num2 + "substracted from" + num1 + "? ");
+                answer = sc.nextInt();
+                correctanswer=num1-num2;
+                break;
+            case 4:
+                System.out.println("What is " + num1 + " divided by " + num2 + "? ");
+                answer = sc.nextInt();
+                correctanswer=num1/num2;
+                break;
+        }
+
+        if(answer==correctanswer){
+            System.out.println("Correct");
+        }
+        else {
+            System.out.println("Wrong. The correct answer is " + correctanswer);
+        }
+
             
-            System.out.println("What is " + num1 + " times " + num2 + "? ");
-            int answer = sc.nextInt();
-            
-            if(answer == (num1 + num2)) {
-                System.out.println("You're correct!");
-            } else {
-                System.out.println("Incorrect.");
-            }
+           
         } while(true);
         sc.close();
     }
